f3893f195c92f76f0b4ffa079365f06b238da652
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-01-24 08:04:38 +0300

    preload.c: handle the case of hdrp=NULL better

--- preload.c
+++ preload.c
@@ -31,37 +31,43 @@ rpmRC rpmReadPackageFile(rpmts ts, FD_t fd, const char *fn, Header *hdrp)
 	if (!hdrcache_key(fname, &st, &key))
 	    fname = NULL;
     }
-    // the caller may pass hdrp=NULL, but we want to fetch the header anyway
-    Header hdr_;
-    if (hdrp == NULL)
-	hdrp = &hdr_;
     // get from the cache
     if (fname) {
 	unsigned off;
-	*hdrp = hdrcache_get(&key, &off);
-	if (*hdrp) {
+	Header h = hdrcache_get(&key, &off);
+	if (h) {
 	    int pos = lseek(Fileno(fd), off, SEEK_SET);
 	    if (pos != (int) off)
-		*hdrp = headerFree(*hdrp);
-	    else
+		headerFree(h);
+	    else {
+		if (hdrp)
+		    *hdrp = h;
+		else
+		    headerFree(h);
 		return RPMRC_OK;
+	    }
 	}
     }
-    // call the real __func__
+    // set up and call the real __func__
     static
     rpmRC (*next)(rpmts ts, FD_t fd, const char *fn, Header *hdrp);
     if (next == NULL) {
 	next = dlsym(RTLD_NEXT, __func__);
 	assert(next);
     }
-    rpmRC rc = next(ts, fd, fn, hdrp);
+    Header h = NULL;
+    rpmRC rc = next(ts, fd, fn, fname ? &h : hdrp);
     // put to the cache
     if (fname) {
 	if (rc == RPMRC_OK || rc == RPMRC_NOTTRUSTED || rc == RPMRC_NOKEY) {
 	    int pos = lseek(Fileno(fd), 0, SEEK_CUR);
 	    if (pos > 0)
-		hdrcache_put(&key, *hdrp, pos);
+		hdrcache_put(&key, h, pos);
 	}
+	if (hdrp)
+	    *hdrp = h;
+	else if (h)
+	    headerFree(h);
     }
     return rc;
 }
